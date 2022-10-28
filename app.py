04cd464ab6f963cfe4e1ecccb8cc0130129854aa
Sahand <sanaeisahand@gmail.com>
2022-10-28 19:10:58 +0330

    initiation

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from itertools import zip_longest
+
+
+
+# Configure application
+app = Flask(__name__)
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///database.db")
+
+
+@app.route("/")
+def register():
+    return render_template('/register.html')  
